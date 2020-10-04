fdf2e73be55d280a4aa1a0bc4b61942391b0852f
lazarte1991 <71897549+lazarte1991@users.noreply.github.com>
2020-10-04 20:29:48 -0300

    Add files via upload

--- app/Model/CategoriaModel.php
+++ app/Model/CategoriaModel.php
@@ -13,19 +13,26 @@
             $sentencia->execute();
             return  $sentencia->fetchAll(PDO::FETCH_OBJ);
         }
+
+        function GetCategoriaPorColeccion($coleccion){
+            $sentencia = $this->db->prepare("SELECT id FROM categoria where coleccion=?");
+            $sentencia->execute(array($coleccion));
+            return  $sentencia->fetchAll(PDO::FETCH_OBJ);
+        }
+
         function InsertCategoria($url_img,$coleccion){
-            $sentencia = $this->db->prepare( "INSERT INTO categoria(url_img,coleccion) VALUES(?,?)");           
+            $sentencia = $this->db->prepare("INSERT INTO categoria(url_img,coleccion) VALUES(?,?)");           
             $sentencia->execute(array($url_img,$coleccion));
         }
         
         function DeleteCategoria($id_categoria){
-            $sentencia = $this->db->prepare( "DELETE FROM categoria WHERE id=?");
+            $sentencia = $this->db->prepare("DELETE FROM categoria WHERE id=?");
             $sentencia->execute(array($id_categoria));
         }
         
-        function EditCategoria($id_categoria){
-            $sentencia = $this->db->prepare( "UPDATE categoria SET url_img='$_POST[url_img]',coleccion='$_POST[coleccion]' WHERE id=?");
-            $sentencia->execute(array($id_categoria));
+        function EditCategoria($id_categoria, $url_img, $coleccion){
+            $sentencia = $this->db->prepare("UPDATE categoria SET url_img=?,coleccion=? WHERE id=?");
+            $sentencia->execute(array($url_img,  $coleccion, $id_categoria));
         }
         
 
--- app/Model/ProductoModel.php
+++ app/Model/ProductoModel.php
@@ -12,23 +12,22 @@
             $sentencia = $this->db->prepare( "SELECT * FROM producto WHERE id_categoria=?");
             $sentencia->execute(array($id));
             return  $sentencia->fetchAll(PDO::FETCH_OBJ);
-           
         }
 
-        function InsertProducto($color,$id_categoria,$talle,$tipo){
-            $sentencia = $this->db->prepare( "INSERT INTO producto(color,id_categoria,talle,tipo) VALUES(?,?,?,?)");           
-            $sentencia->execute(array($color,$id_categoria,$talle,$tipo));
+        function InsertProducto($color,$talle,$tipo){
+            $sentencia = $this->db->prepare("INSERT INTO producto(color,talle,tipo) VALUES(?,?,?)");           
+            $sentencia->execute(array($color,$talle,$tipo));
             
         }
 
         function DeleteProducto($id){
-            $sentencia = $this->db->prepare( "DELETE FROM producto WHERE id=?");
+            $sentencia = $this->db->prepare("DELETE FROM producto WHERE id=?");
             $sentencia->execute(array($id));
         }
 
-        function EditProducto($id,$color,$id_categoria,$talle,$tipo){
-            $sentencia = $this->db->prepare( "UPDATE producto SET color=?,id_categoria=?,talle=?,tipo=? WHERE id=?");
-            $sentencia->execute(array($color,$id_categoria,$talle,$tipo,$id));
+        function EditProducto($id,$color,$talle,$tipo){
+            $sentencia = $this->db->prepare("UPDATE producto SET tipo=?, color=?, talle=? WHERE id=?");
+            $sentencia->execute(array($tipo,$color,$talle,$id));
         }
         
     }
