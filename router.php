7f3b2d11e628c7a4c190f15001a2c1379cafb926
lazarte1991 <71897549+lazarte1991@users.noreply.github.com>
2020-10-04 20:27:06 -0300

    Add files via upload

--- router.php
+++ router.php
@@ -1,40 +1,31 @@
 <?php 
-
-     require_once 'app/Controller/indumentariaController.php';
-    // //require_once 'Controller/indumentariaAdvanceController.php';
-     require_once 'RouterClass.php';
-     if(!empty($_GET['action'])) {
+    define("BASE_URL", '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']).'/');
+    require_once('app/Controller/indumentariaController.php');
+    require_once 'RouterClass.php';
+    if(!empty($_GET['action'])) {
         $action = $_GET['action'];
     }else {
         $action = "home";
     }
-    // // CONSTANTES PARA RUTEO
-     define("BASE_URL", 'http://'.$_SERVER["SERVER_NAME"].':'.$_SERVER["SERVER_PORT"].dirname($_SERVER["PHP_SELF"]).'/');
+    $r = new Router(); 
 
-     $r = new Router();
-    // //////////////////////////////PENDIENTE////////////////////////////////////////
-    // // rutas
-     $r->addRoute("home", "GET", "indumentariaController", "Home");
-    // //$r->addRoute("mermelada", "GET", "TasksController", "Home");
+    $r->addRoute("home", "GET", "indumentariaController", "Home");
+    $r->addRoute("categorias", "GET", "indumentariaController", "Categorias");
 
-    // //Esto lo veo en TasksView
-     $r->addRoute("insert", "POST", "indumentariaController", "InsertCategoria");
-     $r->addRoute("delete/:ID", "GET", "indumentariaController", "DeleteCategoria");
-     $r->addRoute("edit/:ID", "POST", "indumentariaController", "EditCategoria");
-     $r->addRoute("ver/:ID", "GET", "indumentariaController", "VerCategoria");
+    // $r->addRoute("home", "GET", "indumentariaController", "showProducto");
+    
+    $r->addRoute("categoria/insertProducto", "POST", "indumentariaController", "insertProductoEnCategoria");
+    $r->addRoute("categoria/deleteProducto/:ID", "GET", "indumentariaController", "deleteProducto");
+    $r->addRoute("categoria/editProducto/:ID", "POST", "indumentariaController", "editProducto");
 
-     //$r->addRoute("delete/:ID", "GET", "IndumentariaController", "BorrarLaTaskQueVienePorParametro");
-    // $r->addRoute("completar/:ID", "GET", "IndumentariaController", "MarkAsCompletedTask");
+    $r->addRoute("insertCategoria", "POST", "indumentariaController", "insertCategoria");
+    $r->addRoute("deleteCategoria/:ID", "GET", "indumentariaController", "deleteCategoria");
+    $r->addRoute("editCategoria/:ID", "POST", "indumentariaController", "editCategoria");
+    $r->addRoute("categoria/:COLECCION", "GET", "indumentariaController", "showProducto");
 
-    // //Ruta por defecto.
-    // $r->setDefaultRoute("IndumentariaController", "Home");
+    $r->setDefaultRoute("IndumentariaController", "Home");
 
-    // //Advance
-    // $r->addRoute("advance", "GET", "IndumentariaAdavanceController", "Mostrar");
-    // $r->addRoute("autocompletar", "GET", "IndumentariaAdavanceController", "AutoCompletar");
 
-    // //run
-     $r->route($_GET['action'], $_SERVER['REQUEST_METHOD']); 
-    // ////////////////////////////////////////////////////////////////////////////////
+    $r->route($_GET['action'], $_SERVER['REQUEST_METHOD']); 
 ?>
 
