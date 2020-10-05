81c603a0f3aa6386fdf12c74b4d47aa5caf8636a
lazarte1991 <71897549+lazarte1991@users.noreply.github.com>
2020-10-05 20:35:20 -0300

    Add files via upload

---
+++ app/View/indumentariaView.php
@@ -0,0 +1,54 @@
+<?php
+
+require_once('libs/smarty/libs/Smarty.class.php');
+
+    class IndumentariaView {
+
+        private $title;
+
+        function __construct() {
+            $this->title  = "Subject";
+        }
+
+        function showHome(){
+            $smarty = new Smarty();
+          //  $smarty->assign('categorias', $categorias);
+            $smarty->display('templates/home.tpl');
+            
+        }
+
+        function showCategorias($categorias){
+            $smarty = new Smarty();
+            $smarty->assign('categorias', $categorias);
+            $smarty->display('templates/categorias.tpl');
+            
+        }
+
+        function showProducto($categoria, $producto){
+            $smarty = new Smarty();
+            $smarty->assign('categoria', $categoria, true);
+            $smarty->assign('producto', $producto, true);
+            $smarty-> display('templates/tabla.tpl');
+        }
+        
+
+        // function showProductosPorCategoria($productos){
+        //     $smarty = new Smarty();
+        //     $smarty->assign('productos', $productos);
+        //     $smarty->display('templates/productos_por_categoria.tpl');
+        // }
+
+
+    
+        function ShowHomeLocation(){
+            header("Location: ".BASE_URL."home");
+        }
+
+        function ShowCategoriasNombreLocation($coleccion){
+            header("Location: ".BASE_URL."home/categorias/".$coleccion);
+        }
+        function ShowCategoriasIDLocation($id_coleccion){
+            header("Location: ".BASE_URL."home/categorias/".$id_coleccion);
+        }
+    }
+?>
