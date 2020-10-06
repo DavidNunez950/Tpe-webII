ef84ca541d30793469de9e32b677d34a56075672
DavidNunez950 <davidlanuez@gmail.com>
2020-10-06 10:46:33 -0300

    martes 6 de ictubre: jutadas las dos ramas

--- app/Controller/indumentariaController.php
+++ app/Controller/indumentariaController.php
@@ -19,27 +19,27 @@
         // 1.a Funciones para ver categorias
 
         function Home() {
-            $this->checkLoggedIn();
+            // $this->checkLoggedIn();
             $categoria = $this->CategoriaModel->GetCategoria();
             $this->view->showHome($categoria);
         }
         // 1.b Funciones para realizar acciones de ABM con la tabla de categorias
 
         function insertCategoria(){
-            $this->checkLoggedIn();
+            // $this->checkLoggedIn();
             $this->CategoriaModel-> InsertCategoria($_POST['input_url_img'],$_POST['input_coleccion']);
             $this->view->ShowHomeLocation();
         } 
 
         function deleteCategoria($params = null){
-            $this->checkLoggedIn();
+            // $this->checkLoggedIn();
             $id_categoria = $params[':ID'];
             $this->CategoriaModel->DeleteCategoria($id_categoria);
             $this->view->ShowHomeLocation();
         }
 
         function editCategoria($params = null){
-            $this->checkLoggedIn();
+            // $this->checkLoggedIn();
             $id_categoria = $params[':ID'];
             $url_img = $_POST['url_img'];
             $coleccion= $_POST['coleccion'];
@@ -49,7 +49,7 @@
 
         // 2.a Funciones para ver categorias
         function showProducto($params = null){
-            $this->checkLoggedIn();
+            // $this->checkLoggedIn();
             $id_categoria = $params[':ID'];
             $categoria =  $this->CategoriaModel->GetCategoriaPorID($id_categoria);
             $producto = $this->ProductoModel->GetProducto($id_categoria);
@@ -58,7 +58,7 @@
 
         // 2.b Funciones para realizar acciones de ABM con productos
         function insertProductoEnCategoria($params = null){
-            $this->checkLoggedIn();
+            // $this->checkLoggedIn();
             $id_categoria = $params[':ID'];
             if(isset($_POST['color'])&&isset($_POST['talle'])&&isset($_POST['tipo'])) {
                 $this->ProductoModel->InsertProducto($_POST['color'], $_POST['talle'], $_POST['tipo'], $id_categoria);
@@ -67,7 +67,7 @@
         }
 
         function editProducto($params = null){
-            $this->checkLoggedIn();
+            // $this->checkLoggedIn();
             $id_producto = $params[':ID'];
             if(isset($_POST['color'])&&isset($_POST['talle'])&&isset($_POST['tipo'])) {
                 $this->ProductoModel->EditProducto($id_producto, $_POST['color'], $_POST['talle'], $_POST['tipo']);
@@ -76,15 +76,14 @@
         }
 
         function deleteProducto($params = null){
-            $this->checkLoggedIn();
+            // $this->checkLoggedIn();
             $id_producto = $params[':ID'];
             $this->ProductoModel->DeleteProducto($id_producto);
             $this->view->ShowHomeLocation();
         }
 
         function Categorias() {
-            $this->checkLoggedIn();
-            $this->checkLoggedIn();
+            // $this->checkLoggedIn();
             $categoria = $this->CategoriaModel->GetCategoria();
             $this->view->showCategorias($categoria);
         }
--- app/View/UserView.php
+++ app/View/UserView.php
@@ -12,11 +12,11 @@ class UserView{
     }
 
     function ShowLogin($message = ""){
-
         $smarty = new Smarty();
-        $smarty->assign('titulo_s', $this->title);
+        $smarty->assign('titulo_s', $this->title, true);
         $smarty->assign('message', $message);
-
+        $smarty->assign('BASE_URL', BASE_URL, true);
+        var_dump(BASE_URL);
         $smarty->display('templates/login.tpl'); // muestro el template 
     }
 
