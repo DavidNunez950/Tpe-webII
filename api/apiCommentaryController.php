ceecd3600d242320ef5ddf86b5877596740df6ca
DavidNunez950 <davidlanuez@gmail.com>
2020-11-13 15:33:41 -0300

    Viernes 13 de noviembre: mejorado el código de la api para evitar errores

--- api/apiCommentaryController.php
+++ api/apiCommentaryController.php
@@ -19,35 +19,45 @@ class ApiCommentaryController extends ApiController {
     
     }
 
-    function deleteCommentary($params = null){
-        $id_commentary = $params[':ID'];
-        $this->checkForComment($id_commentary);
-        $this->model->deleteCommentary($id_commentary);
-        $this->view->response("Se borro", 200);
-    }
-
     function insertCommentary(){
         $body = $this->getData();
-        $id = $this->model->insertCommentary($body->text,$body->star,$body->date,$body->id_product,$body->id_user);     
-        $commentary = $this->model->getCommentaryById($id);
-        if(empty($commentary)) {
-            $this->view->response("Cant insert commentary", 500);
-        } else{
-            $this->view->response($commentary, 200);
-        } 
+        try {
+            $id = $this->model->insertCommentary($body->text,$body->star,$body->date,$body->id_product,$body->id_user);     
+            $commentary = $this->model->getCommentaryById($id);
+            $status = 200;
+            if(empty($commentary)) {
+                $commentary = "Cant insert commentary";
+                $status = 500;
+            }
+            $this->view->response($commentary, $status);
+        } catch(exception $e) {
+            $this->view->response("Internal server error", 500);
+        }  
+    }
+
+    function deleteCommentary($params = null){
+        $id_commentary = $params[':ID'];
+        $commentary = $this->model->getCommentaryById($id_commentary);
+        $this->checkIfResorseisNotFound($commentary, $id_commentary);
+        $this->model->deleteCommentary($id_commentary);
+        $this->view->response("Succesfuly deleted", 200);
     }
 
     function editCommentary($params = null){
         $id_commentary = $params[':ID'];
         $body = $this->getData();
-        $this->checkForComment($id_commentary);      
-        $this->model->updateCommentary($body->text,$body->star,$id_commentary);
-        $this->view->response("Se edito", 200);
-
+        $commentary = $this->model->getCommentaryById($id_commentary);
+        $this->checkIfResorseisNotFound($commentary, $id_commentary);
+        try {
+            $this->model->updateCommentary($body->text,$body->star,$id_commentary);
+            $this->view->response("Succesfuly updated", 200);
+        } catch(exception $e) {
+            $this->view->response("Internal server error", 500);
+        }  
     }
 
-    function checkForComment($id){
-        if (empty($this->model->getCommentaryById($id))){
+    function checkIfResorseisNotFound($commentary, $id){
+        if (empty($commentary)){
             $this->view->response("Not found commentary with id=$id", 404);
             die();
         }
