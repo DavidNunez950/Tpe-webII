ca57d98806853b7c50c9825b9ea2600391b298df
lazarte1991 <lazarteleandro5@gmail.com>
2020-11-19 12:18:03 -0300

    Jueves 19 de noviembre: se modifico el formulario de insertar comentario

--- component/commentaries.vue
+++ component/commentaries.vue
@@ -2,7 +2,7 @@
 
 <div class="col-12 bg-white" id="commentary">
     <ul class="list-group">
-        <li class="list-group-item" v-for="commentary in commentaries">{{commentary.text}}</li>
+        <li class="list-group-item list-group-item-secondary " v-for="commentary in commentaries">{{commentary.text}}</li>
 
     </ul>
 </div>
